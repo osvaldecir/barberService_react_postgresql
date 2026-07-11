548c846515e3e5c432aee5e416a9ca59cb2a3bfc
osvaldecir <manocatanduva@gmail.com>
2026-07-11 05:19:26 -0300

    //

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
-import { Geist } from "next/font/google";
-import { cn } from "@/_utils";
 
-const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
+
+import "./globals.css";
+import { cn } from "@/utils";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -28,11 +29,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
+    <html lang="en" className={cn("font-sans", geistSans.variable)}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+         
+          geistSans.variable,
+          geistMono.variable,
+          "antialiased"
+        )}
       >
-        <h1>ola</h1>
+        <h1>Olá</h1>
         {children}
       </body>
     </html>
